307995e5370acba378e39dc0c431961c3d05f0e2
Erick1528 <ejosuemenjivar.15@gmail.com>
2026-02-11 22:07:08 +0100

    Update favicon in admin layout from SVG to WEBP format for improved performance

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ asset('build/assets/logo.svg') }}">
+    <link rel="icon" href="{{ asset('build/assets/logo.webp') }}">
     <title>@yield('title') - Admin - Daphne Breeze</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
