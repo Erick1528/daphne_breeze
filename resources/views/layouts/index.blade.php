e8fbc9170772f5a4279bd68752e29816d5669e53
Erick1528 <ejosuemenjivar.15@gmail.com>
2026-02-18 13:33:31 +0100

    Add Cookiebot script for enhanced cookie consent management in homepage layout
    - Integrated Cookiebot script to manage user consent for cookies, ensuring compliance with privacy regulations.
    - Updated the layout to include the new script, improving user data management and consent handling.

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,6 +9,9 @@
     <link rel="icon" href="{{ asset('build/assets/logo.webp') }}">
     <title>Daphne Breeze - @yield('title')</title>
 
+    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="a72fdd9b-3167-4abd-aebf-43a3ffecab14"
+        data-blockingmode="auto" type="text/javascript"></script>
+
     <script
         data-cookieconsent="ignore">    window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag("consent", "default", { ad_personalization: "denied", ad_storage: "denied", ad_user_data: "denied", analytics_storage: "denied", functionality_storage: "denied", personalization_storage: "denied", security_storage: "granted", wait_for_update: 500, }); gtag("set", "ads_data_redaction", true); gtag("set", "url_passthrough", false);</script>
 
